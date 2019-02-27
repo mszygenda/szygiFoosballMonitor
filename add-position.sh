54d7ad22cb3de55e7aa57a92e73a4170f29b7689
Mateusz Szygenda <mateusz.szygenda@senti1.com>
2019-02-27 17:12:19 +0100

    moves code around, adds more testing utils

--- add-position.sh
+++ add-position.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+RANDOM_VALUE=$(($RANDOM % 500))
+VALUE=${1:-$RANDOM_VALUE}
 
-curl localhost:3000/position -X POST --data "{\"value\": $(($RANDOM % 500))}" -H 'Content-Type: application/json'
+curl localhost:3000/position -X POST --data "{\"value\": $VALUE}" -H 'Content-Type: application/json'
 echo
---
+++ start-position-stream.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while [ 0 ]; do
+	./add-position.sh "$@"
+	sleep 1
+done
